ce4569b3cb0aadbf296d53e64b888a007677f937
tobiloba <elteecute@gmail.com>
2022-09-08 17:33:48 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
@@ -23,5 +15,3 @@ int main(void)
 		return (0);
 
 }
-
-
