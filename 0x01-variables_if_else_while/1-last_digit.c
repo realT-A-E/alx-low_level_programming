1a71f34c71b7f4c6047cfce178e47a2bc0e99373
realTAE <elteecute>
2022-09-10 01:11:37 -1200

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,39 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - print last digit in random number
- *
- * Description: print last digit in random number
- *
- * Return: Always 0 (Success)  
- */
-
+* main - Print the alphabet in lowercase letters, except for e and q
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
+		char ex;
 
-	int n, last;
-
-
-	srand(time(0));
-
-	n = rand() - RAND_MAX / 2;
-
-	last = n % 10;
-
-	if (last > 5)
-
-		printf("Last digit of %d is %d and is greater than 5\n", n, last);
-
-	else if (last == 0)
-
-		printf("Last digit of %d is %d and is 0\n", n, last);
-
-	else if (last < 6 && last != 0)
-
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+		for (ex = 'a'; ex <= 'z'; ex++)
+		{
+		if (ex != 'e' && ex != 'q')
+		putchar(ex);
+		}
+		putchar('\n');
 
 	return (0);
-
 }
