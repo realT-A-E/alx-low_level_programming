46f1f5d01289bdcc2373de4fe0944fcef3b6635a
realTAE <elteecute>
2022-09-10 00:55:35 -1200

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,37 +1,20 @@
 #include <stdio.h>
 
-
-
 /**
-
- * main - Print the alphabet in lowercase letters, except for e and q
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
+* main - Print the alphabet in lowercase letters, except for e and q
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-  
 {
-  
-  char ex; 
-  
-  for (ex = 'a'; ex <= 'z'; ex++)
-    
-   {
-      
-      if (ex != 'e' && ex != 'q')
-	
-	putchar(ex);
-      
-    }
-  
-  putchar('\n');
-  
+		char ex;
 
-  
-  return (0);
-  
+		for (ex = 'a'; ex <= 'z'; ex++)
+		{
+		if (ex != 'e' && ex != 'q')
+		putchar(ex);
+		}
+		putchar('\n');
+
+	return (0);
 }
