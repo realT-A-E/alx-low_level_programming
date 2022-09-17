5cdb2bda5177ac73cf4b4b90f0e7570176f783d6
realTAE <elteecute>
2022-09-16 19:19:04 -1200

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a < 100; a++)
+		if (a % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}
